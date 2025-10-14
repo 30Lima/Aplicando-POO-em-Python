f429b40580880c3ff86ae7edb7a267eed18023aa
30lima <pedrolimaa354@gmail.com>
2025-10-13 21:52:37 -0300

    feat: criando classe para aprendizagem

---
+++ modelos/musica.py
@@ -0,0 +1,13 @@
+# Hora da prática: primeiro módulo
+
+class musica:
+    nome = ''
+    artista = ''
+    duracao = ''
+
+vacations = musica()
+vacations.nome = 'Day dreamin'
+vacations.artista = 'Vacations'
+vacations.duracao = '1:55'
+
+print(vars(vacations))
