498998e134fc6b497fcec68e2a4da6bb7acf6b07
30lima <pedrolimaa354@gmail.com>
2025-10-13 21:51:56 -0300

    feat: adicionando primeira classe em python

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,1 +1,14 @@
-print("teste")ads
+class restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = restaurante()
+restaurante_praca.nome = 'Praça'
+restaurante_praca.categoria = 'Gourmet'
+
+#print(restaurante_praca) -> exibição da alocação na memória
+#print(dir(restaurante_praca)) -> todas informações do objeto
+#print(vars(restaurante_praca)) -> exibição do conteúdo da classe em um dicionário
+
+#print(restaurante_praca.ativo) -> exibição de um objeto expecífico
